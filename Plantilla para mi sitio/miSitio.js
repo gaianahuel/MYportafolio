bbbebf0a702f1915389d7a6ead2f0288273003ac
gaianahuel <114672231+gaianahuel@users.noreply.github.com>
2023-06-03 14:00:09 -0300

    Add files via upload
    Mi página oficial

---
+++ Plantilla para mi sitio/miSitio.js
@@ -0,0 +1,10 @@
+
+
+function  sacarRaiz (arreglo) {
+
+ let num= arreglo;
+     for (const element of arreglo) {
+    console.info("la raiz cuadrada de " + num + " es " + element * element);
+   }
+}
+sacarRaiz([3,5,4]);
